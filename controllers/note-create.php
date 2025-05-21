bcf5251f40f9724c4aba6fbf83b1cf8133322f10
beltranbot <beltranbot@gmail.com>
2025-05-21 14:43:54 -0500

    extract a simple validator class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -8,10 +8,21 @@ $heading = "Create note";
 $currentUserId = 1;
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $db->query("insert into notes(body, user_id) values (:body, :user_id)", [
-        "body" => $_POST["body"],
-        "user_id" => $currentUserId,
-    ]);
+    $errors = [];
+
+    if (strlen(trim($_POST["body"])) === 0) {
+        $errors["body"] = "A body is required";
+    }
+    if (strlen(trim($_POST["body"])) > 1000) {
+        $errors["body"] = "The body can not be more than 1,000 characters.";
+    }
+
+    if (empty($errors)) {
+        $db->query("insert into notes(body, user_id) values (:body, :user_id)", [
+            "body" => $_POST["body"],
+            "user_id" => $currentUserId,
+        ]);
+    }
 }
 
 require "views/note-create.view.php";
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -11,13 +11,21 @@
                 <div class="border-b border-gray-900/10 pb-12">
 
                     <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        
+
 
                         <div class="col-span-full">
                             <label for="body" class="block text-sm/6 font-medium text-gray-900">Note</label>
                             <div class="mt-2">
                                 <textarea name="body" id="body" rows="3"
-                                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+                                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    placeholder="Write a note..."
+                                    required><?= $_POST["body"] ?? "" ?></textarea>
+
+                                <?php if (isset($errors["body"])) : ?>
+                                    <p class="text-red-500 text-xs mt-2">
+                                        <?= $errors["body"] ?>
+                                    </p>
+                                <?php endif ?>
                             </div>
                             <p class="mt-3 text-sm/6 text-gray-600">Write a note.</p>
                         </div>
